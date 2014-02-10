167cfb42d5907d4b5010fdc1da3b7b03d9f1ef45
lkkeepmoving <lkkeepmoving@gmail.com>
2014-02-10 00:09:03 -0800

    Update format

--- BitSet.java
+++ BitSet.java
@@ -1,25 +1,25 @@
 public class BitSet {
-	int[] bitset;
+    int[] bitset;
 	
-	public BitSet(int size) {
-		bitset = new int[(size >> 5) + 1];
-	}
+    public BitSet(int size) {
+	bitset = new int[(size >> 5) + 1];
+    }
 	
-	public void set(int pos) {
-		int wordNumber = (pos >> 5); // divide by 32
-		int bitNumber = (pos & 0x1F); // mod 32
-		bitset[wordNumber] |= (1 << bitNumber);
-	}
+    public void set(int pos) {
+	int wordNumber = (pos >> 5); // divide by 32
+	int bitNumber = (pos & 0x1F); // mod 32
+	bitset[wordNumber] |= (1 << bitNumber);
+    }
 	
-	public void unset(int pos) {
-		int wordNumber = (pos >> 5); // divide by 32
-		int bitNumber = (pos & 0x1F); // mod 32
-		bitset[wordNumber] &= ~(1 << bitNumber);
-	}
+    public void unset(int pos) {
+	int wordNumber = (pos >> 5); // divide by 32
+	int bitNumber = (pos & 0x1F); // mod 32
+	bitset[wordNumber] &= ~(1 << bitNumber);
+    }
 	
-	public boolean get(int pos) {
-	    int wordNumber = (pos >> 5); // divide by 32
-	    int bitNumber = (pos & 0x1F); // mod 32
-	    return (bitset[wordNumber] & (1 << bitNumber)) != 0;      
-	}
+    public boolean get(int pos) {
+    	int wordNumber = (pos >> 5); // divide by 32
+    	int bitNumber = (pos & 0x1F); // mod 32
+    	return (bitset[wordNumber] & (1 << bitNumber)) != 0;      
+    }
 }
